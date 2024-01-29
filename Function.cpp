cec124301012bf6c0700871802dd8feeea90eec5
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-01-30 02:01:17 +0900

    確認課題

--- Function.cpp
+++ Function.cpp
@@ -9,7 +9,7 @@ Matrix4x4 MakeRotateAxisAngle(const Vector3& axis, float angle)
 	
 
 	result.m[0][0] = (axis.x * axis.x) * (1.0f - cosAngle) + cosAngle;
-	/*result.m[0][1] = (axis.x * axis.y) * (1.0f - cosAngle) - axis.z * sinAngle;
+	result.m[0][1] = (axis.x * axis.y) * (1.0f - cosAngle) - axis.z * sinAngle;
 	result.m[0][2] = (axis.x * axis.z) * (1.0f - cosAngle) + axis.y * sinAngle;
 										   
 	result.m[1][0] = (axis.x * axis.y) * (1.0f - cosAngle) + axis.z * sinAngle;
@@ -18,7 +18,7 @@ Matrix4x4 MakeRotateAxisAngle(const Vector3& axis, float angle)
 										   
 	result.m[2][0] = (axis.x * axis.z) * (1.0f - cosAngle) - axis.y * sinAngle;
 	result.m[2][1] = (axis.y * axis.z) * (1.0f - cosAngle) + axis.x * sinAngle;
-	result.m[2][2] = (axis.z * axis.z) * (1.0f - cosAngle) + cosAngle;*/
+	result.m[2][2] = (axis.z * axis.z) * (1.0f - cosAngle) + cosAngle;
 	return result;
 }
 
